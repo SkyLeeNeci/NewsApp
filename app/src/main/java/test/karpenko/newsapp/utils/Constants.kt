7dde1e64c8f131298bc8e6aad14f93be98ed01e2
VladyslavKarpenko <92163391+SkyLeeNeci@users.noreply.github.com>
2022-09-05 16:15:24 +0300

    create constants class

---
+++ app/src/main/java/test/karpenko/newsapp/utils/Constants.kt
@@ -0,0 +1,9 @@
+package test.karpenko.newsapp.utils
+
+class Constants {
+    companion object{
+        const val BASE_URL = "https://newsapi.org/"
+        const val API_KEY = "bfed1a79471e43da9971f4c81433dc70"
+
+    }
+}
