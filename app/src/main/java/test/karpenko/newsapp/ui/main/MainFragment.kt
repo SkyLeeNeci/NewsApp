df173f252f99c0046ec04cb19d968b50c82e41ff
VladyslavKarpenko <92163391+SkyLeeNeci@users.noreply.github.com>
2022-09-05 11:18:21 +0300

    create all fragments

---
+++ app/src/main/java/test/karpenko/newsapp/ui/main/MainFragment.kt
@@ -0,0 +1,19 @@
+package test.karpenko.newsapp.ui.main
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import test.karpenko.newsapp.R
+
+class MainFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+}
