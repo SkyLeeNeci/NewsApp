095431f7d6fa43f3cdf173d7bcfaa001a4e6a218
VladyslavKarpenko <92163391+SkyLeeNeci@users.noreply.github.com>
2022-09-06 09:20:25 +0300

    add article id and news response

--- app/src/main/java/test/karpenko/newsapp/data/api/NewsService.kt
+++ app/src/main/java/test/karpenko/newsapp/data/api/NewsService.kt
@@ -1,7 +1,9 @@
 package test.karpenko.newsapp.data.api
 
+import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
+import test.karpenko.newsapp.models.NewsResponse
 import test.karpenko.newsapp.utils.Constants.Companion.API_KEY
 
 interface NewsService {
@@ -11,12 +13,12 @@ interface NewsService {
         @Query("q") query: String,
         @Query("page") page: Int = 1,
         @Query("apiKey") apiKey: String = API_KEY
-    )
+    ): Response<NewsResponse>
 
     @GET("/v2/top-headlines")
     suspend fun getTopHeadlines(
         @Query("country") countryCode: String = "ua",
         @Query("page") page:Int = 1,
         @Query("apiKey") apiKey: String = API_KEY
-    )
+    ):Response<NewsResponse>
 }
