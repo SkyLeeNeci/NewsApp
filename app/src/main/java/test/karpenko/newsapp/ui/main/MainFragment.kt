8fb6ae7f348bd0f714847616572dea09e0afd307
VladyslavKarpenko <92163391+SkyLeeNeci@users.noreply.github.com>
2022-09-07 10:13:56 +0300

    add top news data loading and display it

--- app/src/main/java/test/karpenko/newsapp/ui/main/MainFragment.kt
+++ app/src/main/java/test/karpenko/newsapp/ui/main/MainFragment.kt
@@ -1,12 +1,17 @@
 package test.karpenko.newsapp.ui.main
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import dagger.hilt.android.AndroidEntryPoint
 import test.karpenko.newsapp.databinding.FragmentMainBinding
+import test.karpenko.newsapp.ui.adapters.NewsAdapter
+import test.karpenko.newsapp.utils.Resource
 
 @AndroidEntryPoint
 class MainFragment : Fragment() {
@@ -15,6 +20,9 @@ class MainFragment : Fragment() {
     private val mBinding
         get() = _binding!!
 
+    private val viewModel by viewModels<MainViewModel>()
+    lateinit var newsAdapter: NewsAdapter
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -22,4 +30,46 @@ class MainFragment : Fragment() {
         _binding = FragmentMainBinding.inflate(layoutInflater, container,false)
         return mBinding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initAdapter()
+        observeViewModel()
+    }
+
+    private fun initAdapter() {
+        newsAdapter = NewsAdapter()
+        mBinding.newsRecyclerView.apply {
+            adapter = newsAdapter
+            layoutManager = LinearLayoutManager(activity)
+        }
+
+    }
+
+    private fun observeViewModel(){
+
+        viewModel.newsLiveData.observe(viewLifecycleOwner){ response ->
+            when(response){
+                is Resource.Success -> {
+                    mBinding.progressBar.visibility = View.INVISIBLE
+                    response.data?.let {
+                        newsAdapter.differ.submitList(it.articles)
+                        Log.d("Loading News", "${it.articles}")
+                    }
+                }
+                is Resource.Error -> {
+                    mBinding.progressBar.visibility = View.INVISIBLE
+                    response.data?.let {
+                        Log.d("Loading News", "Main Fragment error: ${it}")
+                    }
+                }
+                is Resource.Loading -> {
+                    mBinding.progressBar.visibility = View.VISIBLE
+                    Log.d("Loading News", "Main Fragment Loading")
+                }
+            }
+        }
+
+    }
+
 }
--- app/src/main/java/test/karpenko/newsapp/ui/main/MainViewModel.kt
+++ app/src/main/java/test/karpenko/newsapp/ui/main/MainViewModel.kt
@@ -1,10 +1,41 @@
 package test.karpenko.newsapp.ui.main
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import test.karpenko.newsapp.data.api.NewsRepository
+import test.karpenko.newsapp.models.NewsResponse
+import test.karpenko.newsapp.utils.Resource
 import javax.inject.Inject
 
 @HiltViewModel
-class MainViewModel @Inject constructor() : ViewModel() {
+class MainViewModel @Inject constructor( private val repository: NewsRepository ) : ViewModel() {
+
+    private val _newsLiveData: MutableLiveData<Resource<NewsResponse>> = MutableLiveData()
+    val newsLiveData: LiveData<Resource<NewsResponse>>
+    get() = _newsLiveData
+
+    private var newsPage = 1
+
+    init {
+        getTopNews("ua")
+    }
+
+    private fun getTopNews(countryCode: String){
+        viewModelScope.launch {
+            _newsLiveData.postValue(Resource.Loading())
+            val response = repository.getTopNews(countryCode, newsPage)
+            if (response.isSuccessful){
+                response.body().let {
+                    _newsLiveData.postValue(Resource.Success(it))
+                }
+            }else {
+                _newsLiveData.postValue(Resource.Error(message = response.message()))
+            }
+        }
+    }
 
 }
